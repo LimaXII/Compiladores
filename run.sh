e4b1a4b94187498fb3be720f1fff31a4bf93f021
Matheus Almeida <71467895+matheusalsi@users.noreply.github.com>
2024-05-05 16:15:08 -0300

    update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/bash
 
+if [ $# -ne 1 ]; then
+    echo "Uso: $0 <arquivo_de_entrada>"
+    exit 1
+fi
+
 TEST_PATH=./e3_tests
 
-source output2dot.sh < teste.txt > $TEST_PATH/saida.dot
-dot -Tpng $TEST_PATH/saida.dot -o $TEST_PATH/saida.png
+ARQUIVO_ENTRADA=$1
+
+source output2dot.sh < "$ARQUIVO_ENTRADA" > "$TEST_PATH/saida.dot"
+dot -Tpng "$TEST_PATH/saida.dot" -o "$TEST_PATH/saida.png"
